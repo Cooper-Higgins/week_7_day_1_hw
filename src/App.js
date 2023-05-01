9689ac910312fd11d29b5304fadb9a3661f37948
Cooper-Higgins <jamie@supplantree.com>
2023-05-01 14:48:01 +0100

    MVP working, radio buttons in place but not working properly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [tasks, setTasks] = useState([
+    {name: "Buy Shopping", highPriority: false},
+    {name: "Clean Bathroom", highPriority: false},
+    {name: "Cancel Economist", highPriority: true}
+  ])
+
+  const [newTask, setNewTask] = useState("")
+
+  const taskNodes = tasks.map((task, index) => {
+    return(
+      <li key={index} className={task.priority ? "high-priority" : "low-priority"}>
+      <span>{task.name}</span>
+      </li>
+    )
+  })
+  
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault()
+    const copyTasks = [...tasks]
+    copyTasks.push({name: newTask, highPriority: newTask})
+    setTasks(copyTasks)
+    setNewTask("")
+  }
+
+  const handlePriorityInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <br></br>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor="new-task">Add a new task:</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}/>
+        <input type="radio" id="high" name="priority" value={true} onChange={handlePriorityInput}/>
+        <label for="high">High</label>
+        <input type="radio" id="low" name="priority" value={false} onChange={handlePriorityInput}/>
+        <label for="low">Low</label>
+        <input type="submit" value="Save Task"/>      
+      </form>
+
+      <ul>
+        {taskNodes}
+      </ul>
+
     </div>
   );
 }
